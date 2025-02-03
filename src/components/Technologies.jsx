0d29ad0877e1253c060293d38061bf5b84ad664f
David Orozco <dav.llwyr@gmail.com>
2025-02-02 18:53:35 -0600

    add language text support in EN and ES

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -11,8 +11,10 @@ import {
 } from "react-icons/fa";
 import { SiSpringboot, SiMysql } from "react-icons/si";
 import { DiMsqlServer } from "react-icons/di";
+import { useLanguage } from "../context/Language";
 
 function Technologies() {
+  const {language} = useLanguage()
   const technologies = [
     { name: "JavaScript", icon: <FaJs className="text-yellow-400" /> },
     { name: "Node.js", icon: <FaNodeJs className="text-green-500" /> },
@@ -26,10 +28,19 @@ function Technologies() {
     { name: "Github", icon: <FaGithub  className="text-black" /> },
   ];
 
+  const text = {
+    en:{
+      title: "Technologies I Know",
+    },
+    es:{
+      title: "Tecnologías que Conozco",
+    }
+  }
+
   return (
     <section className="bg-gradient-to-l from-blue-600 to-blue-900 text-white p-8 md:p-20 ">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-6">My Tech Stack</h2>
+        <h2 className="text-3xl font-bold mb-6">{text[language].title}</h2>
         <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 justify-items-center">
           {technologies.map((tech, index) => (
             <div
