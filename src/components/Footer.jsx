7da34e3bf66f3c844c9024e9513cda0d64d4d5ee
David Orozco <dav.llwyr@gmail.com>
2025-02-02 19:15:46 -0600

    add language text support in EN and ES

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
+import { useLanguage } from "../context/Language";
+
+
+
 const Footer = () => {
+  const {language} = useLanguage();
+
+  const text = {
+    en: {
+      message: "David Orozco. All rights reserverd.",
+      contact: "Contact",
+    },
+    es: {
+      message: "David Orozco. Todos los derechos reservados.",
+      contact: "Contacto",
+    },
+  };
+
     return (
       <footer className="bg-blue-900 text-white py-6 text-center">
         <div className="container mx-auto px-4">
-          <p className="text-sm">&copy; {new Date().getFullYear()} David Orozco. All rights reserverd.</p>
+          <p className="text-sm">&copy; {new Date().getFullYear()} {text[language].message}</p>
           <div className="flex justify-center space-x-6 mt-3">
             <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition">
               GitHub
@@ -11,7 +28,7 @@ const Footer = () => {
               LinkedIn
             </a>
             <a href="mailto:tucorreo@example.com" className="hover:text-blue-300 transition">
-              Contact
+              {text[language].contact}
             </a>
           </div>
         </div>
