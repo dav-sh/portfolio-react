8f3bfcd2b95c0ed5bcb4a8e35846407ef92164a7
David Orozco <dav.llwyr@gmail.com>
2025-02-02 20:05:46 -0600

    add language text support in EN and ES

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useLanguage } from "../context/Language";
 
 function ProjectCard({ img, title, desc }) {
+  const {language} = useLanguage()
+
+  const text = {
+    en: {
+      button_git: "Github"
+    },
+    es: {
+      button_git: "Github"
+    }
+  }
   return (
     <div className="max-w-sm mx-auto border-2 border-blue-400 rounded-lg shadow-lg overflow-hidden">
       <img
@@ -14,7 +25,7 @@ function ProjectCard({ img, title, desc }) {
         <h2 className="text-2xl font-semibold text-center text-blue-800">{title}</h2>
         <p className="text-gray-700 text-center my-2">{desc}</p>
         <button className="w-full bg-blue-800 text-white font-semibold cursor-pointer hover:bg-blue-500 rounded-2xl px-4 py-2 transition-all duration-300">
-          Ver más
+          {text[language].button_git}
         </button>
       </div>
     </div>
